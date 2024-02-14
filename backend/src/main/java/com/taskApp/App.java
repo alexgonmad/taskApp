5ed14b70c53d2957f45742f123510b674b27cba4
Alex GM <xelagm990@gmail.com>
2024-02-14 13:39:22 +0100

    testing scanner class import

--- backend/src/main/java/com/taskApp/App.java
+++ backend/src/main/java/com/taskApp/App.java
@@ -1,13 +1,20 @@
 package com.taskApp;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor, ingresa una cadena de texto: ");
+        
+        String input = scanner.nextLine();
+        System.out.println("La cadena de texto ingresada es: " + input);
+
+        scanner.close();
     }
 }
